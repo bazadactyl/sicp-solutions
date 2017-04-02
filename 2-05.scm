9aff518e9e723c1912e658ca5c1cdb3ab27e9c0e
Basim Ramadhan <basim.ramadhan@gmail.com>
2017-04-02 08:32:34 -0400

    Added ex2.5

---
+++ 2-05.scm
@@ -0,0 +1,16 @@
+#lang sicp
+
+(define (cons a b)
+  (lambda (m) (m (expt 2 a)
+                 (expt 3 b))))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(car (cons 2 2))
+(cdr (cons 2 2))
+(car (cons 3 3))
+(cdr (cons 3 3))     

cfe218c77d6b9a5295c5ae4208d2f6d028b052be
Basim Ramadhan <basim.ramadhan@gmail.com>
2017-04-01 22:04:44 -0400

    Added ex2.4

---
+++ 2-04.scm
@@ -0,0 +1,22 @@
+#lang sicp
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(car (cons x y))
+(car (lambda (m) (m x y)))
+((lambda (m) (m x y)) (lambda (p q) p))
+((lambda (p q) p) x y)
+x
+
+(cdr (cons x y))
+(cdr (lambda (m) (m x y)))
+((lambda (m) (m x y)) (lambda (p q) q))
+((lambda (p q) q) x y)
+y
